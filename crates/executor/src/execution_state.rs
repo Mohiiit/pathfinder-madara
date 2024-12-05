f2adf792208b3305479c85397f60db59bd6881d4
mohiiit <mohit18343@iiitd.ac.in>
2024-12-05 21:34:17 +0530

    native token address updated

--- crates/executor/src/execution_state.rs
+++ crates/executor/src/execution_state.rs
@@ -24,7 +24,7 @@ use crate::IntoStarkFelt;
 pub const ETH_FEE_TOKEN_ADDRESS: ContractAddress =
     contract_address!("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7");
 pub const STRK_FEE_TOKEN_ADDRESS: ContractAddress =
-    contract_address!("0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d");
+    contract_address!("0xe1b2c0e3fe25b00d641a64cf8450f096121c96fec9bfd2ddbfba0baee54b84");
 
 mod versioned_constants {
     use std::borrow::Cow;
